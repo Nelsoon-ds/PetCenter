a6b76d85932514797f04da331fb95bcf76361b33
Bishobos <mps.kajkage@gmail.com>
2025-09-15 15:14:43 +0200

    Emptied the main

--- src/PetAdoptionCenter.java
+++ src/PetAdoptionCenter.java
@@ -1,11 +1,13 @@
 public class PetAdoptionCenter {
 
     public static void main(String[] args) {
+        doer();
+    }
+
+    public static void doer(){
         PetShop sop = new PetShop();
         Pet[] arr = sop.makePets();
         sop.showPets();
     }
-
-    //
 }
 
