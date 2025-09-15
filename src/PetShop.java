7a85370af22ba3b925f431447dbb9f03d74426e0
Bishobos <mps.kajkage@gmail.com>
2025-09-15 15:23:43 +0200

    Made some things more readable

--- src/PetShop.java
+++ src/PetShop.java
@@ -81,7 +81,7 @@ public class PetShop {
     }
 
     public void showPets(){
-        System.out.println("======Pets Registered======");
+        System.out.println("=====List of registered pets=====");
         for (Pet pet : this.inventory) {
             System.out.println("Pet number 1: ");
             System.out.println(pet.toString());
